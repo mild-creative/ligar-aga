519a19c98e7182d3a057b7a64ed6b0d3d84a6763
hafrizresa <hafrizresa@gmail.com>
2020-09-21 18:35:29 +0700

    add description

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,13 @@ export const opener = {
   brand: 'Powered by Mild Creative',
   bride: 'Ligar',
   groom: 'Aga',
-  description: 'Sehubungan dengan adanya wabah covid-19 dan dalam rangka mengikuti himbauan dari pemerintah, kami hanya mengadakan acara akad nikah yang dihadiri keluarga. Semoga Bapak/Ibu, Saudara/i dan Kerabat sekalian dapat memakluminya. Mudah-mudahan wabah Covid 19 segera berlalu dan kita semua selalu diberi kesehatan. Terima kasih.',
+  description: `Dengan segala kerendahan hati izinkan kami berbagi momen bahagia dalam pernikahan kami.
+
+  Tanpa mengurangi rasa hormat dalam rangka mengikuti himbauan dari pemerintah terkait pandemi Covid-19, kami hanya melangsungkan acara Akad Nikah yang dihadiri keluarga.
+  
+  Semoga Bapak/Ibu, Saudara/i dan Kerabat sekalian dapat memakluminya.
+  
+  Terima kasih atas doanya.`,
   top: 'Official Wedding Website'
 };
 
